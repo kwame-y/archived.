451cdee7f7b5ad378c48635611f060c8def5c533
Kwame Yeboah <kwameyeboah@Mac.attlocal.net>
2026-01-07 20:59:56 -0600

    Quartz sync: Jan 7, 2026, 8:59 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "kwame-y.github.io/archived.",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
